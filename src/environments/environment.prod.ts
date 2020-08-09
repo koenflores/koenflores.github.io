23580ec657fa8b3b715bb0e24452000f8e74f9b5
Gianne <gianneflores18@gmail.com>
2020-08-09 12:49:27 -0700

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAK7sM8m5UsKOh7e8v-KLL3HAeVJu93JrQ",
+    authDomain: "xxxx.firebaseapp.com",
+    databaseURL: "https://xxxx.firebaseio.com",
+    projectId: "kenonsblog",
+    storageBucket: "xxxx.appspot.com",
+    messagingSenderId: "473359610657"
+  }
 };
